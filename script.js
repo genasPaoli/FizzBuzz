55570a004e32e26884cdf07c89e992ad84432112
Genaro Paoli <mgenasp2403@gmail.com>
2025-11-19 19:25:18 -0600

    feat: agrego el codigo de fizzbuzz y lo enlazo en html

---
+++ script.js
@@ -0,0 +1,15 @@
+//Usamos la funcion for para  comenzar un blucle, agregamos la funcion let para asignarle 
+//al iterador i que comience valiendo 1  y se debe de detener en 1000 e indicamos que el interador debe de ir incrementando en 1 
+// usando i++
+
+
+for (let i = 1 ; i<1000 ; i++) 
+    if (i % 3 === 0 && i % 5 === 0) {
+        
+    console.log ("FizzBuzz") } else if (i % 3 === 0 ) {
+
+    console.log ("Fizz") } else if (i % 5 === 0) {
+        console.log ("Buzz")} 
+    else {console.log (i)}
+        
+    
