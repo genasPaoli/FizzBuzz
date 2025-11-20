de50491168fcbc15fb9f81571cef91e8b0a0aaba
Genaro Paoli <mgenasp2403@gmail.com>
2025-11-19 19:30:08 -0600

    fix: correccion para que el conteo llegue a 1000 y no sea menor a 1000

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // usando i++
 
 
-for (let i = 1 ; i<1000 ; i++) 
+for (let i = 1 ; i<=1000 ; i++) 
     if (i % 3 === 0 && i % 5 === 0) {
         
     console.log ("FizzBuzz") } else if (i % 3 === 0 ) {
